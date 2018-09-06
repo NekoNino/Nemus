fde3b04a07546776cb2d330477644e4530debc8a
Minh Nilsson <myapple@minhs-macbook-air.local>
2018-09-06 13:33:26 +0200

    Testing stuct

--- Nemus Apocalypse/MainGameViewController.swift
+++ Nemus Apocalypse/MainGameViewController.swift
@@ -8,9 +8,17 @@
 
 import UIKit
 
+struct Stage {
+    var bgImage:String
+    var text:String
+    let choice:[(text: String,nextStage: Int)]
+    
+}
+
 class MainGameViewController: UIViewController {
 
-    ///MARK: - Outlets
+    @IBOutlet weak var imageView: UIImageView!
+    // MARK: - Outlets
     @IBOutlet weak var text: UITextView!
     
     //Button 1
@@ -29,17 +37,33 @@ class MainGameViewController: UIViewController {
 //        testText.text = "Write you name"
      //   print("Show text")
         
-        
+    
         choiceA.setTitle("Choice A", for: .normal)
         choiceB.setTitle("Choice B", for: .normal)
         choiceC.setTitle("Choice C", for: .normal)
         
-    
+        var stage = Stage(bgImage: "bild", text: "Scenario-text",
+                          choice: [
+                            ("Choice 1", 3),
+                            ("Choice 2", 8),
+                            ("Choice 3", 6)
+            ])
+        
+        loadStage(stage: stage)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func loadStage(stage:Stage) {
+        text.text = stage.text
+        imageView.image = UIImage(named: stage.bgImage)
+        choiceA.setTitle(stage.choice[0].text, for: .normal)
+        choiceB.setTitle(stage.choice[1].text, for: .normal)
+        choiceC.setTitle(stage.choice[2].text, for: .normal)
+        
     }
     
 
@@ -53,14 +77,15 @@ class MainGameViewController: UIViewController {
     }
     */
     
-    ///MARK: - Actions
+    // MARK: - Actions
     
-    @IBAction func choiceAPress(_ sender: UIButton) {
+    
+    /*@IBAction func choiceAPress(_ sender: UIButton) {
         print("Button A pressed")
-        loadState(stageDescription: "Choice A pressed!", choiceAText: "Choice A 2", choiceBText: "Choice B 2", choiceCText: "Choice C 2")
+        loadState(stageDescription: "Choice B pressed", choiceAText: "Choice A 2", choiceBText: "Choice B 2", choiceCText: "Choice C 2")
     }
     
-    @IBAction func choiceBPress(_ sender: UIButton) {
+   @IBAction func choiceBPress(_ sender: UIButton) {
         text.text = "Button B pressed"
         choiceA.setTitle("Choice A 3", for: .normal)
         choiceB.setTitle("Choice B 3", for: .normal)
@@ -78,5 +103,6 @@ class MainGameViewController: UIViewController {
         choiceC.setTitle(choiceCText, for: .normal)
 
     }
-    
+ 
+ */
 }
